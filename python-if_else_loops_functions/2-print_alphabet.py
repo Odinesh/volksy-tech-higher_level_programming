49c4a1c8e91a470310d0f0d3fcedef3ad862dd69
Odinesh <dinesho@brightcomgroup.com>
2023-01-01 21:54:06 -0800

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,17 +2,18 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
- *
- * Return: Always 0 (Succes)
- */
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
-	int n;
+	char c;
 
-	for (n = 'a'; n <= 'z'; n++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(n);
+		_putchar(c);
 	}
+
 	_putchar('\n');
 }
