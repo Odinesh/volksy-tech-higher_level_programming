43be1915a773ff42f409aa5ca12f552791bdca06
Odinesh <dinesho@brightcomgroup.com>
2023-01-01 22:53:40 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3  
 for i in range(ord('a'), ord('z')+1):
-    a = chr()
-    if i not in "qe":
-        print(letter, end="" .format(chr))
+    a = chr(i)
+    if a not in "qe":
+        print(a, end="" .format(chr))
