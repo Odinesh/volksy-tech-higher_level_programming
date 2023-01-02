85ee090a8ab1b5afe5955042dc0d738b4e87351c
Odinesh <dinesho@brightcomgroup.com>
2023-01-01 22:11:17 -0800

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-#include "holberton.h"
-
-/**
-  * print_alphabet - Make the alphabet
-  *
-  * Return: void
-  */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-
-	_putchar('\n');
-}
+for i in a range(a, z):
+    print(chr(i), end=" ")
