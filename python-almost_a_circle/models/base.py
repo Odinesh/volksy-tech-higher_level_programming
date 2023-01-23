de576c77562fa79ceebb4826be23d8119983a6f0
Odinesh <dinesho@brightcomgroup.com>
2023-01-23 01:32:10 -0800

    __init__.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""base"""
+
+
+class Base():
+    """base class for checking the id for others class"""
+
+    
+    __nb_objects = 0
+
+
+    def __init__(self,id=None):
+        """initialization base class with id"""
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects += 1
+            self.id = Base.__nb_objects
