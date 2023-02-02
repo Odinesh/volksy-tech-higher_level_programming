2891d615efb4e01bd45d90d0a0f0589a35bbf501
Odinesh <dinesho@brightcomgroup.com>
2023-02-01 21:51:47 -0800

    MY SQL

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privilage.
-SHOW GAINT FOR 'user_0d_1' WHERE IN 'localhost';
-SHOW GAINT FOR 'user_0d_2' WHERE IN 'localhost';
+SHOW GRANTS FOR 'user_0d_1' WHERE IN 'localhost';
+SHOW GRANTS FOR 'user_0d_2' WHERE IN 'localhost';
