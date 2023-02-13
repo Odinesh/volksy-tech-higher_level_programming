86559330611201fe7b84988dbcfe9257e21651d0
Odinesh <dinesho@brightcomgroup.com>
2023-02-13 03:10:48 -0800

    100-relationship_states_cities.py

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Creates State and City for hbtn_0e_100_usa database
 """
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 class City(Base):
     """ City class inherits Base sqlalchemy call
     """
+
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
