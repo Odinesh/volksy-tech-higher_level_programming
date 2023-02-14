8429f1faa26525569c5531bd7279714a5f5d871e
Odinesh <dinesho@brightcomgroup.com>
2023-02-13 21:05:57 -0800

    relationship_city.py

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" City class model
 """
-from relationship_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+class definition of a City and an instance of Base
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """ City class inherits Base sqlalchemy call
     """
-
-    __tablename__ = 'cities'
+    city class
+    """
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128))
-    state_id = Column(Integer, ForeignKey('states.id'))
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
