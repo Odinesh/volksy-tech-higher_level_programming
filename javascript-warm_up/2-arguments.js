54dff78591088ff48d9068d5755415ff8bedb5cd
Odinesh <dinesho@brightcomgroup.com>
2023-02-21 20:55:24 -0800

    arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length < 3) {
+  console.log('no argument');
+} else if (process.argv.length == 3) {
+  console.log('argument found');
+} else {
+  consol.log('arguments found');
+}
