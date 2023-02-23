c3f20c24f36c99a90fc951bbec59e13a058f1075
Odinesh <dinesho@brightcomgroup.com>
2023-02-22 20:20:49 -0800

    4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+// class Rectangle that defines a rectangle
+class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      Object.create(Rectangle);
-    } else if (w && h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    let y = 'X';
     for (let i = 0; i < this.height; i++) {
-      console.log(y.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    let i = this.width;
+    const aux = this.width;
     this.width = this.height;
-    this.height = i;
+    this.height = aux;
   }
 
   double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
