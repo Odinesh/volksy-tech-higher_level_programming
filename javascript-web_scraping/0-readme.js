80302c2813f8730a2980bd9b8619c8913a5b5a5d
Odinesh <dinesho@brightcomgroup.com>
2023-02-23 21:01:05 -0800

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let fs = require('fs');
-let file = process.argv[2];
+const fs = require('fs');
+const file = process.argv[2];
 
 function readPrint (file) {
   fs.readFile(file, 'utf-8', function (err, data) {
